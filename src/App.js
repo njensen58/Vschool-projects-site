9a55b3a469f3bf10fe342af45899be2ecba0b310
Nate Jensen <nathan.jensen.58@gmail.com>
2019-02-22 11:52:46 -0700

    spa-transition-added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import { Switch, Route, withRouter } from 'react-router-dom'
 import Home from './components/Home.js'
 import ProjectList from './components/ProjectList.js'
 import routesData from './static-data/routes.json'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import './styles/index.css'
+import './styles/transitions.css'
 
 const App = props => {
+    const { location } = props
     return (
         <div className="app-container">
-            <Switch>
-                <Route exact path="/"             render={ rProps => <Home {...rProps}/> }/>
-                { routesData.routes.map(({path, listTitle, listDescription, projectsArr}, i) => 
-                    <Route path={path} key={i} render={rProps => 
-                        <ProjectList 
-                            {...rProps} 
-                            listName={listTitle} 
-                            listDescription={listDescription} 
-                            projectsArr={projectsArr}/>}/>
-                )}
-            </Switch>
+            <TransitionGroup>
+                <CSSTransition
+                    in={false}
+                    appear={false}
+                    key={location.key}
+                    timeout={400}
+                    classNames="page-fade"
+                >
+                    <Switch location={location}>
+                        <Route exact path="/"             render={ rProps => <Home {...rProps}/> }/>
+                        { routesData.routes.map(({path, listTitle, listDescription, projectsArr}, i) => 
+                            <Route path={path} key={i} render={rProps => 
+                                <ProjectList 
+                                    {...rProps} 
+                                    listName={listTitle} 
+                                    listDescription={listDescription} 
+                                    projectsArr={projectsArr}/>}/>
+                        )}
+                    </Switch>
+                </CSSTransition>
+            </TransitionGroup>
             <footer>
                 <span>V School</span>
                 <div onClick={() => props.history.goBack()}>Back</div>
